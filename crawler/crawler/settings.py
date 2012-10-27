75780c574fb54a6a481c5b2d0fa7b9d1c65856c0
Bruno Polaco Penteado <polaco@gmail.com>
2012-10-27 21:06:01 -0200

    Adjusting speed because we all respect wikipedia

--- crawler/crawler/settings.py
+++ crawler/crawler/settings.py
@@ -11,5 +11,7 @@ BOT_NAME = 'crawler'
 SPIDER_MODULES = ['crawler.spiders']
 NEWSPIDER_MODULE = 'crawler.spiders'
 
+DOWNLOAD_DELAY = 0.25
+
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'crawler (+http://www.yourdomain.com)'
