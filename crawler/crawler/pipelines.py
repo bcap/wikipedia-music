bb7f335653abc6875632e4d53647d3d17f6dba15
Bruno Polaco Penteado <polaco@gmail.com>
2012-10-27 22:10:24 -0200

    Adding a pipeline for generating dot files to be used on graphviz

--- crawler/crawler/pipelines.py
+++ crawler/crawler/pipelines.py
@@ -3,6 +3,31 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/topics/item-pipeline.html
 
-class CrawlerPipeline(object):
+class DotPipeline(object):
+
+    def __init__(self):
+        self.file = open('result.dot', 'wb')
+        self.file.write('digraph wikipedia_music {\n')
+
     def process_item(self, item, spider):
+        for origin in item['origins']:
+            self.write_relation(origin['name'], item['name'])
+
+        for subgenre in item['subgenres']: 
+            self.write_relation(item['name'], subgenre['name'])
+
+        #for derivative in item['derivatives']:
+        #    self.write_relation(item['name'], derivative['name'])
+
         return item
+
+    def write_relation(self, source, destination, metadata = ''):
+        self.file.write('    "' + source + '" -> "' + destination + '"')
+        if len(metadata) > 0:
+            self.file.write(' [' + metadata + ' ]')
+        self.file.write(';\n')
+        self.file.flush()
+
+    def close_spider(self, spider):
+        self.file.write('}')
+        self.file.close()
