3704da03104e12b1bbf24e043f561e9b682b8475
Bruno Polaco Penteado <polaco@gmail.com>
2012-10-28 17:21:33 -0200

    Ranndom arrow coloring and workaround (bad) for avoiding duplicated relations. Need to investigate it further

--- crawler/crawler/pipelines.py
+++ crawler/crawler/pipelines.py
@@ -3,13 +3,19 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/topics/item-pipeline.html
 
+import random
+from sets import Set
+
 class DotPipeline(object):
+
+    writen = Set()
 
     def __init__(self):
         self.file = open('result.dot', 'wb')
         self.file.write('digraph wikipedia_music {\n')
 
     def process_item(self, item, spider):
+
         for origin in item['origins']:
             self.write_relation(origin['name'], item['name'])
 
@@ -21,12 +27,22 @@ class DotPipeline(object):
 
         return item
 
-    def write_relation(self, source, destination, metadata = ''):
-        self.file.write('    "' + source + '" -> "' + destination + '"')
-        if len(metadata) > 0:
-            self.file.write(' [' + metadata + ' ]')
-        self.file.write(';\n')
-        self.file.flush()
+    def write_relation(self, source, destination):
+        relation = source + destination
+        
+        # bad workaround that wastes memory to solve uninvestigated bug where relations get duplicated. Will do it later with a rested mind
+        if relation not in self.writen:
+            self.file.write('    "' + source + '" -> "' + destination + '" [color="' + self.randomColor() + '"];\n')
+            self.file.flush()
+            self.writen.add(relation)
+
+    def randomColor(self):
+        """generates a visible random color on a white background on rgb format (#rrggbb)"""
+        red = random.randint(1, 200)
+        green = random.randint(1, 200)
+        blue = random.randint(1, 200)
+        return '#%02X%02X%02X' % (red, green, blue)
+
 
     def close_spider(self, spider):
         self.file.write('}')
